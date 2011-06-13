defe5c97414f39b787490b05c088678762b6d629
Meinte Boersma <meinte.boersma@gmail.com>
2011-06-13 17:51:12 +0200

    documented Xtensions and fixed meta model issue

--- nl.dslmeinte.xtext.util/src/nl/dslmeinte/xtext/generation/EmfExtensions.java
+++ nl.dslmeinte.xtext.util/src/nl/dslmeinte/xtext/generation/EmfExtensions.java
@@ -2,12 +2,23 @@ package nl.dslmeinte.xtext.generation;
 
 import org.eclipse.emf.ecore.EObject;
 
+/**
+ * A util class to hold Java Xtensions.
+ * 
+ * @author Meinte Boersma
+ */
 public class EmfExtensions {
 
 	private EmfExtensions() {
 		// prevent instantiation...
 	}
 
+	/**
+	 * @param eObject
+	 *            - a model element
+	 * @return the file name of the {@link Resource} the given {@link EObject}
+	 *         resides in, without extension.
+	 */
 	public static String fileNameWithoutExtension(EObject eObject) {
 		String uri = eObject.eResource().getURI().toString();
 		return uri.substring(uri.lastIndexOf('/') + 1, uri.lastIndexOf('.'));
