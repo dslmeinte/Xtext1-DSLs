3c7bcf43a9043c3abad30f92cb52f626057ee698
Meinte Boersma <meinte.boersma@gmail.com>
2011-07-06 14:05:19 +0200

    got the standalone parser working correctly

--- nl.dslmeinte.xtext.sgml.test.simplemarkup/src/nl/dslmeinte/xtext/sgml/test/simplemarkup/parser/CustomizedInternalSimpleMarkupParser.java
+++ nl.dslmeinte.xtext.sgml.test.simplemarkup/src/nl/dslmeinte/xtext/sgml/test/simplemarkup/parser/CustomizedInternalSimpleMarkupParser.java
@@ -1,9 +1,12 @@
 package nl.dslmeinte.xtext.sgml.test.simplemarkup.parser;
 
+import nl.dslmeinte.xtext.parser.util.ParsingOverrideUtil;
 import nl.dslmeinte.xtext.sgml.test.simplemarkup.parser.antlr.internal.InternalSimpleMarkupParser;
 import nl.dslmeinte.xtext.sgml.test.simplemarkup.services.SimpleMarkupGrammarAccess;
 import nl.dslmeinte.xtext.sgml.test.simplemarkup.services.SimpleMarkupGrammarAccess.Reference_attributesElements;
 
+import org.antlr.runtime.CommonToken;
+import org.antlr.runtime.MismatchedTokenException;
 import org.antlr.runtime.RecognitionException;
 import org.antlr.runtime.Token;
 import org.antlr.runtime.TokenStream;
@@ -11,6 +14,7 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtext.parser.IAstFactory;
 import org.eclipse.xtext.parser.antlr.XtextTokenStream;
 import org.eclipse.xtext.parser.antlr.XtextTokenStream.HiddenTokens;
+import org.eclipse.xtext.parsetree.LeafNode;
 
 public class CustomizedInternalSimpleMarkupParser extends InternalSimpleMarkupParser {
 
@@ -36,22 +40,24 @@ public class CustomizedInternalSimpleMarkupParser extends InternalSimpleMarkupPa
 			match(input, 21, FOLLOW_21_in_ruleReference_attributes1125);
 			createLeafNode(metadata.getEqualsSignKeyword_1(), null);
 			// QUOTED_STRING, which will be re-interpreted as '"' to=[Section|IDENTIFIER] '"':
-			match(input, RULE_QUOTED_STRING, null);
+			if( input.LA(1) != RULE_QUOTED_STRING ) {
+				throw new MismatchedTokenException(RULE_QUOTED_STRING, input);
+			}
+			input.consume();
 
-//			match(input, 22, FOLLOW_22_in_ruleReference_attributes1135);
-//			createLeafNode(metadata.getQuotationMarkKeyword_2(), null);
 			current = factory.create(getGrammarAccess().getReference_attributesRule().getType().getClassifier());
 			// remove surrounding quotes from token text:
-			Token token = input.LT(-1);
+			CommonToken token = (CommonToken) input.LT(-1);
 			String quotedString = token.getText();
-			token.setText(quotedString.substring(1, quotedString.length()-1));
+			quotedString = quotedString.substring(1, quotedString.length()-1);
 			// TODO  really un-escape the quoted string
-			
-
+			Token leftQuoteToken = ParsingOverrideUtil.createReplacementToken(token, 22, "\"", 0);
+			createLeafNode(metadata.getQuotationMarkKeyword_2(), null, leftQuoteToken, true);
+			Token identifierToken = ParsingOverrideUtil.createReplacementToken(token, RULE_IDENTIFIER, quotedString, 1);
 			associateNodeWithAstElement(currentNode, current);
-			createLeafNode(metadata.getToSectionCrossReference_3_0(), "to");
-//			match(input, 22, FOLLOW_22_in_ruleReference_attributes1163);
-//			createLeafNode(metadata.getQuotationMarkKeyword_4(), null);
+			createLeafNode(metadata.getToSectionCrossReference_3_0(), "to", identifierToken, false);
+			Token rightQuoteToken = ParsingOverrideUtil.createReplacementToken(token, 22, "\"", quotedString.length() + 2);
+			createLeafNode(metadata.getQuotationMarkKeyword_4(), null, rightQuoteToken, true);
 
 			resetLookahead();
 			lastConsumedNode = currentNode;
@@ -65,27 +71,25 @@ public class CustomizedInternalSimpleMarkupParser extends InternalSimpleMarkupPa
 		return current;
 	}
 
-//	protected Object createLeafNode(EObject grammarElement, String feature) {
-////		Token token = new CommonT
-//		Token token = input.LT(-1);
-//		if (token != null && token.getTokenIndex() > lastConsumedIndex) {
-//			int indexOfTokenBefore = lastConsumedIndex;
-//			if (indexOfTokenBefore + 1 < token.getTokenIndex()) {
-//				for (int x = indexOfTokenBefore + 1; x < token.getTokenIndex(); x++) {
-//					Token hidden = input.get(x);
-//					LeafNode leafNode = createLeafNode(hidden, hidden.getChannel() == HIDDEN);
-//					setLexerRule(leafNode, hidden);
-//				}
-//			}
-//			LeafNode leafNode = createLeafNode(token, false);
-//			leafNode.setGrammarElement(grammarElement);
-//			leafNode.setFeature(feature);
-//			lastConsumedIndex = token.getTokenIndex();
-//			lastConsumedNode = leafNode;
-//			tokenConsumed(token, leafNode);
-//			return leafNode;
-//		}
-//		return null;
-//	}
+    /*
+     * {@see AbstractInternalAntlrParser#createLeafNode}: modified copy
+     */
+	protected Object createLeafNode(EObject grammarElement, String feature, Token token, boolean hiddenReplacement) {
+		if (token != null ) {
+			/*
+			 * Note that we don't perform any checks (and promotes) on hidden
+			 * tokens that may be between the last consumed token and the
+			 * given token.
+			 */
+			LeafNode leafNode = ParsingOverrideUtil.createLeafNode(this, token, hiddenReplacement);
+			leafNode.setGrammarElement(grammarElement);
+			leafNode.setFeature(feature);
+			lastConsumedIndex = token.getTokenIndex();
+			lastConsumedNode = leafNode;
+			ParsingOverrideUtil.tokenConsumed(this, token, leafNode);
+			return leafNode;
+		}
+		return null;
+	}
 
 }
