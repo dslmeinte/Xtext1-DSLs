37fe0dd141072ef5722a44bf1328510fadcdc95e
Meinte Boersma <meinte.boersma@gmail.com>
2011-07-07 10:34:41 +0200

    moved generation of SimpleMarkup.xtext from ...sgml.tests project to ...test.simplemarkup project

---
+++ nl.dslmeinte.xtext.sgml.test.simplemarkup/src/nl/dslmeinte/xtext/sgml/test/simplemarkup/GenerateSimpleMarkupGrammar.java
@@ -0,0 +1,65 @@
+package nl.dslmeinte.xtext.sgml.test.simplemarkup;
+
+import java.io.FileNotFoundException;
+
+import nl.dslmeinte.xtext.dtd.DtdLanguageStandaloneSetup;
+import nl.dslmeinte.xtext.dtd.dtdModel.DtdModelPackage;
+import nl.dslmeinte.xtext.sgml.dtd.DTD2XtextTransformer;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.mwe.utils.StandaloneSetup;
+
+public class GenerateSimpleMarkupGrammar {
+
+	static {
+		if( !EPackage.Registry.INSTANCE.containsKey(DtdModelPackage.eNS_URI) ) {
+			DtdLanguageStandaloneSetup.doSetup();
+		}
+		new StandaloneSetup().setPlatformUri("..");
+	}
+
+	private final static String SIMPLE_MARKUP_BASE_PACKAGE_NAME = "nl.dslmeinte.xtext.sgml.test.simplemarkup";
+	private final static String SIMPLE_MARKUP_LANGUAGE_fqName = SIMPLE_MARKUP_BASE_PACKAGE_NAME + ".SimpleMarkup";
+	private final static String SIMPLE_MARKUP_nsUri = "http://dslmeinte.nl/Xtext/sgml/dtd/test/simpleMarkup";
+
+	/**
+	 * Creates a {@code platform:/resource/} {@link URI} into this project from
+	 * a source folder (either {@code "src"} or {@code "src-gen"}) and a postfix
+	 * path.
+	 */
+	private URI platformResourceURI(String sourceFolder, String postfixPath) {
+		return URI.createPlatformResourceURI(
+				String.format("%s/%s/%s/%s", SIMPLE_MARKUP_BASE_PACKAGE_NAME, sourceFolder, SIMPLE_MARKUP_BASE_PACKAGE_NAME.replace('.', '/'), postfixPath),
+				true
+			);
+	}
+
+	private void run() throws FileNotFoundException {
+		DTD2XtextTransformer.transform(
+				platformResourceURI("src", "simpleMarkup.dtd"),
+				SIMPLE_MARKUP_LANGUAGE_fqName,
+				SIMPLE_MARKUP_nsUri,
+				platformResourceURI("src", "SimpleMarkup.xtext"),
+				xpandPackageQualifiedName() + "::FeatureMixin"
+			);
+		modifyGeneratedParsers(platformResourceURI("src-gen", "parser/antlr/internal/InternalSimpleMarkupParser"), "Section_attributes");
+	}
+
+	/**
+	 * Removes the {@code final} keyword for specific parser methods in order to
+	 * be able to sub class the parser class and override those methods.
+	 */
+	private void modifyGeneratedParsers(URI uri, String...ruleNames) {
+		// TODO  implement
+	}
+
+	private String xpandPackageQualifiedName() {
+		return getClass().getPackage().getName().replaceAll("\\.", "::");
+	}
+
+	public static void main(String...args) throws FileNotFoundException {
+		new GenerateSimpleMarkupGrammar().run();
+	}
+
+}
--- nl.dslmeinte.xtext.sgml.tests/src/nl/dslmeinte/xtext/sgml/dtd/test/DTD2XtextTransformerTest.java
+++ nl.dslmeinte.xtext.sgml.tests/src/nl/dslmeinte/xtext/sgml/dtd/test/DTD2XtextTransformerTest.java
@@ -5,7 +5,6 @@ import java.io.FileNotFoundException;
 import nl.dslmeinte.xtext.sgml.dtd.DTD2XtextTransformer;
 import nl.dslmeinte.xtext.sgml.dtd.test.support.DTDTestSupport;
 
-import org.eclipse.emf.common.util.URI;
 import org.junit.Test;
 
 public class DTD2XtextTransformerTest extends DTDTestSupport {
@@ -18,41 +17,6 @@ public class DTD2XtextTransformerTest extends DTDTestSupport {
 				"http://dslmeinte.nl/Xtext/sgml/dtd/test/trivial",
 				createModelsURI("trivial-gen.xtext")
 			);
-	}
-
-	private final static String SIMPLE_MARKUP_BASE_PACKAGE_NAME = "nl.dslmeinte.xtext.sgml.test.simplemarkup";
-	private final static String SIMPLE_MARKUP_LANGUAGE_fqName = SIMPLE_MARKUP_BASE_PACKAGE_NAME + ".SimpleMarkup";
-	private final static String SIMPLE_MARKUP_nsUri = "http://dslmeinte.nl/Xtext/sgml/dtd/test/simpleMarkup";
-
-	private static URI simpleMarkupPlatformResourceURI(String sourceFolder, String postfixPath) {
-		return URI.createPlatformResourceURI(
-				String.format("%s/%s/%s/%s", SIMPLE_MARKUP_BASE_PACKAGE_NAME, sourceFolder, SIMPLE_MARKUP_BASE_PACKAGE_NAME.replace('.', '/'), postfixPath),
-				true
-			);
-	}
-
-	@Test
-	public void test_transformation_of_simple_markup_dtd_with_AOP() throws FileNotFoundException {
-		DTD2XtextTransformer.transform(
-				createModelsURI("simpleMarkup.dtd"),
-				SIMPLE_MARKUP_LANGUAGE_fqName,
-				SIMPLE_MARKUP_nsUri,
-				simpleMarkupPlatformResourceURI("src", "SimpleMarkup.xtext"),
-				xpandPackageQualifiedName() + "::FeatureMixin"
-			);
-		modifyGeneratedParsers(simpleMarkupPlatformResourceURI("src-gen", "parser/antlr/internal/InternalSimpleMarkupParser"), "Section_attributes");
-	}
-
-	/**
-	 * Removes the {@code final} keyword for specific parser methods in order to
-	 * be able to sub class the parser class and override those methods.
-	 */
-	private void modifyGeneratedParsers(URI uri, String...ruleNames) {
-		// TODO  implement
-	}
-
-	private String xpandPackageQualifiedName() {
-		return getClass().getPackage().getName().replaceAll("\\.", "::");
 	}
 
 }
