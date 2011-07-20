37b1a5d9815e2a5adde4001f3c8314796623b8a5
Meinte Boersma <meinte.boersma@gmail.com>
2011-07-20 20:39:30 +0200

    renamed Module class to something less "generic"

--- nl.dslmeinte.xtext.util/src/nl/dslmeinte/xtext/builder/ReadonlyIndifferentClusteringModule.java
+++ nl.dslmeinte.xtext.util/src/nl/dslmeinte/xtext/builder/ReadonlyIndifferentClusteringModule.java
@@ -20,7 +20,7 @@ import com.google.inject.Binder;
  * 
  * @author Meinte Boersma
  */
-public class CustomizedClusteringModule extends ClusteringModule {
+public class ReadonlyIndifferentClusteringModule extends ClusteringModule {
 
 	@Override
 	public void configure(Binder binder) {
