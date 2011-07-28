70a65dec0c0068f90f0b3680bb29e711837ec14d
Meinte Boersma <meinte.boersma@gmail.com>
2011-07-28 10:45:58 +0200

    (added comments)

--- nl.dslmeinte.xtext.sgml.base.ui/src/nl/dslmeinte/xtext/sgml/base/ui/guice/UiModuleSupport.java
+++ nl.dslmeinte.xtext.sgml.base.ui/src/nl/dslmeinte/xtext/sgml/base/ui/guice/UiModuleSupport.java
@@ -12,8 +12,40 @@ import org.eclipse.xtext.ui.editor.syntaxcoloring.antlr.AbstractAntlrTokenToAttr
 import com.google.inject.Binder;
 import com.google.inject.name.Names;
 
+/**
+ * Support class which you can call from the UI Guice module of the UI project
+ * of your Xtext language which is based on the Base SGML base language, to do
+ * the appropriate bindings, like so:
+ * 
+ * <pre>
+ * public void configureHighlightingLexer(Binder binder) {
+ * 	UiModuleSupport.configureHighlightingLexer(binder);
+ * }
+ * 
+ * public void configureContentAssistLexer(Binder binder) {
+ * 	UiModuleSupport.configureContentAssistLexer(binder);
+ * }
+ * 
+ * public Class&lt;? extends AbstractAntlrTokenToAttributeIdMapper&gt; bindAbstractAntlrTokenToAttributeIdMapper() {
+ * 	return UiModuleSupport.bindAbstractAntlrTokenToAttributeIdMapper();
+ * }
+ * 
+ * public void configureIResourceDescriptionsBuilderScope(Binder binder) {
+ * 	UiModuleSupport.configureIResourceDescriptionsBuilderScope(binder);
+ * }
+ * </pre>
+ * 
+ * Use the last configuration only if you're using the clustering Xtext builder,
+ * configured in the UI project's {@code plugin.xml}.
+ * 
+ * @author Meinte Boersma
+ */
 public class UiModuleSupport {
 
+	/**
+	 * Binds the generic SGML lexer (instead of the generated one), for lexing
+	 * for the parsing for the semantic highlighter.
+	 */
 	public static void configureHighlightingLexer(Binder binder) {
 		binder
 			.bind(org.eclipse.xtext.parser.antlr.Lexer.class)
@@ -21,6 +53,10 @@ public class UiModuleSupport {
 			.to(SgmlLexerForParsing.class);
 	}
 
+	/**
+	 * Binds the generic SGML lexer (instead of the generated one), for lexing
+	 * for a parse without model creation for content assist.
+	 */
 	public static void configureContentAssistLexer(Binder binder) {
 		binder
 			.bind(org.eclipse.xtext.ui.editor.contentassist.antlr.internal.Lexer.class)
@@ -28,10 +64,20 @@ public class UiModuleSupport {
 			.to(SgmlLexerForContentAssist.class);
 	}
 
+	/**
+	 * Binds the {@link AbstractAntlrTokenToAttributeIdMapper} which takes care
+	 * of correct highlighting for Xtext languages based on the {@code Base}
+	 * SGML language.
+	 */
 	public static Class<? extends AbstractAntlrTokenToAttributeIdMapper> bindAbstractAntlrTokenToAttributeIdMapper() {
 		return BaseTokenToAttributeIdMapper.class;
 	}
 
+	/**
+	 * Configures the correct {@link IResourceDescriptions} implementation to
+	 * work with the clustering Xtext builder (configured in {@code plugin.xml}
+	 * ).
+	 */
     public static void configureIResourceDescriptionsBuilderScope(Binder binder)
     {  
         binder
--- nl.dslmeinte.xtext.sgml.base/src/nl/dslmeinte/xtext/sgml/base/guice/RuntimeModuleSupport.java
+++ nl.dslmeinte.xtext.sgml.base/src/nl/dslmeinte/xtext/sgml/base/guice/RuntimeModuleSupport.java
@@ -8,8 +8,29 @@ import org.eclipse.xtext.conversion.IValueConverterService;
 import com.google.inject.Binder;
 import com.google.inject.name.Names;
 
+/**
+ * Support class which you can call from the runtime Guice module of the
+ * standalone project of your Xtext language which is based on the Base SGML
+ * base language, to do the appropriate bindings, like so:
+ * 
+ * <pre>
+ * public void configureRuntimeLexer(Binder binder) {
+ * 	RuntimeModuleSupport.configureRuntimeLexer(binder);
+ * }
+ * 
+ * public Class&lt;? extends IValueConverterService&gt; bindIValueConverterService() {
+ * 	return RuntimeModuleSupport.bindIValueConverterService();
+ * }
+ * </pre>
+ * 
+ * @author Meinte Boersma
+ */
 public class RuntimeModuleSupport {
 
+	/**
+	 * Binds the generic SGML lexer (instead of the generated one), for lexing
+	 * for a full parse including model creation.
+	 */
 	public static void configureRuntimeLexer(Binder binder) {
 		binder
 			.bind(org.eclipse.xtext.parser.antlr.Lexer.class)
@@ -17,6 +38,10 @@ public class RuntimeModuleSupport {
 			.to(SgmlLexerForParsing.class);
 	}
 
+	/**
+	 * Makes sure that instances of {@code QuotedString} have their surrounding
+	 * quotation remarks removed from their syntax.
+	 */
 	public static Class<? extends IValueConverterService> bindIValueConverterService() {
 		return BaseTerminalsConverter.class;
 	}
