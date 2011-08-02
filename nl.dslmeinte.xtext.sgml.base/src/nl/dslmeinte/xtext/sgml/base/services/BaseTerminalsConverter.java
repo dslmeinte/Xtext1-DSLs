7a3057f622a521d12d1dc56c3853f806a9df0ea6
Meinte Boersma <meinte.boersma@gmail.com>
2011-08-02 15:26:20 +0200

    Added a value converter to defeat invalidation of proposals.

--- nl.dslmeinte.xtext.sgml.base/src/nl/dslmeinte/xtext/sgml/base/services/BaseTerminalsConverter.java
+++ nl.dslmeinte.xtext.sgml.base/src/nl/dslmeinte/xtext/sgml/base/services/BaseTerminalsConverter.java
@@ -4,13 +4,14 @@ import org.eclipse.xtext.conversion.IValueConverter;
 import org.eclipse.xtext.conversion.ValueConverter;
 import org.eclipse.xtext.conversion.ValueConverterException;
 import org.eclipse.xtext.conversion.impl.AbstractDeclarativeValueConverterService;
+import org.eclipse.xtext.conversion.impl.AbstractLexerBasedConverter;
 import org.eclipse.xtext.parsetree.AbstractNode;
 
 public class BaseTerminalsConverter extends AbstractDeclarativeValueConverterService {
 
 	@ValueConverter(rule = "QuotedString")
 	public IValueConverter<String> QuotedString() {
-		return new IValueConverter<String>(){
+		return new IValueConverter<String>() {
 				@Override
 				public String toString(String value) throws ValueConverterException {
 					return "\"" + value + "\"";
@@ -22,5 +23,30 @@ public class BaseTerminalsConverter extends AbstractDeclarativeValueConverterSer
 				}
 			};
 	}
+
+	/**
+	 * @return A no-op {@link IValueConverter} to prevent the default-mapped
+	 *         {@link AbstractLexerBasedConverter} from raising exceptions due
+	 *         to mismatched token types, because we do our own lexing.
+	 */
+	@ValueConverter(rule = "IDENTIFIER")
+	public IValueConverter<String> Identifier() {
+		return noOpConverter;
+	}
+
+	/**
+	 * Singleton {@link IValueConverter} implementation of the
+	 * "no operation"-/identity-conversion.
+	 */
+	private static IValueConverter<String> noOpConverter = new IValueConverter<String>() {
+		@Override
+		public String toString(String value) throws ValueConverterException {
+			return value;
+		}
+		@Override
+		public String toValue(String string, AbstractNode node) throws ValueConverterException {
+			return string;
+		}
+	};
 
 }
